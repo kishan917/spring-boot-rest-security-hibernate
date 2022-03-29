2e36b253077fa5759fdb901b77569bdbff981ae9
Kishan <36566853+kishan917@users.noreply.github.com>
2022-03-29 16:22:09 +0530

    Add files via upload

---
+++ src/test/java/com/kishan/springboot/testapis/TestApplication.java
@@ -0,0 +1,13 @@
+package com.kishan.springboot.testapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
